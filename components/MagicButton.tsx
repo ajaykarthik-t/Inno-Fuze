24aa99e6c272bfb5fd255225edaec62a9a1856e0
Ajay Karthik <ajaykarthik.eng@gmail.com>
2025-04-16 16:59:50 +0530

    final

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -1,30 +1,25 @@
 import React from "react";
 
-/**
- *  UI: border magic from tailwind css btns
- *  Link: https://ui.aceternity.com/components/tailwindcss-buttons
- *
- *  change border radius to rounded-lg
- *  add margin of md:mt-10
- *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
- */
 const MagicButton = ({
   title,
   icon,
   position,
   handleClick,
   otherClasses,
+  type = "button", // Add default value of "button"
 }: {
   title: string;
   icon: React.ReactNode;
   position: string;
   handleClick?: () => void;
   otherClasses?: string;
+  type?: "button" | "submit" | "reset"; // Add this new prop to the type definition
 }) => {
   return (
     <button
       className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
+      type={type} // Add the type attribute to the button element
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
